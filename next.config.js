56fcb66b76cde065f26d4a1243bd2405b09f7070
jhesid2345 <jhesid2345@gmail.com>
2025-01-26 17:22:24 -0400

    feat: :rocker: Añadir base path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const { BASEPATH } = require("./src/app/constantes");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: BASEPATH,
 };
 
 module.exports = nextConfig;
--- src/app/constantes.ts
+++ src/app/constantes.ts
@@ -1,1 +1,2 @@
 export const SERVICIO = "https://notion-service-spe-bolivia.onrender.com"
+export const BASEPATH = "https://github.com/Jhesid-es/spebolivia"

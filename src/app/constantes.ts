64b21892d8d98e2ef6e2a7dd9319d5ad11076e46
jhesid2345 <jhesid2345@gmail.com>
2025-01-27 09:09:24 -0400

    fix: :hammer: Modificacion minima

--- src/app/constantes.ts
+++ src/app/constantes.ts
@@ -1,5 +1,5 @@
-//export const SERVICIO = "https://notion-service-spe-bolivia.onrender.com"
-export const SERVICIO = "http://localhost:3000"
+export const SERVICIO = "https://notion-service-spe-bolivia.onrender.com"
+//export const SERVICIO = "http://localhost:3000"
 
  export const BASEPATH = "/spebolivia"
 // export const BASEPATH = ""
